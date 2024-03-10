8566a68226fcd3f1f7e72696ebaaa0bc29049c7d
Debasish Panda <144515067+Debash1sh-Panda@users.noreply.github.com>
2024-03-10 12:26:40 +0530

    Create ordersData.js

---
+++ ordersData.js
@@ -0,0 +1,7 @@
+const ordersData = [
+  { id: 1, customer: 'Customer 1', date: '2024-03-10', status: 'Processing' },
+  { id: 2, customer: 'Customer 2', date: '2024-03-11', status: 'Shipped' },
+  // Add more orders as needed
+];
+
+export default ordersData;
